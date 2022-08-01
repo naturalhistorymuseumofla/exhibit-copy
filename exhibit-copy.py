365361639134588a0ea9dd095bbb8bdef6a72df0
Daniel Markbreiter <dmarkbreiter@macbookpro0002.nhm.lac>
2022-08-01 10:38:13 -0700

    Fixed duplicates

--- exhibit-copy.py
+++ exhibit-copy.py
@@ -1,25 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from pathlib import Path
 import argparse
 
 
-def main(old_string, new_string, dir):
+def main(old_string, new_string, dir=None):
+    def return_new_path(path):
+        '''
+        Returns a properly formatted new Path object with proper new location and string prefix
+        :param path: Path object to be formatted
+        :return: Path object
+        '''
+        if path.parent in renames.keys():
+            parent = renames[path.parent]
+        else:
+            parent = path.parent
+        return parent.joinpath(f'{new_string}' + path.name[3:])
+
+    # Keeps track of new paths
     renames = {}
-<<<<<<< HEAD:copy.py
-    dir = Path(dir[0])
-=======
-    dir = Path(dir)
->>>>>>> 20d38777c90b291803b00b5bd23337ca0e2bab8a:exhibit-copy.py
+
+    if dir:
+        dir = Path(dir)
+        # If the optional target dir arg is given, add its new filepath to the renames dictionary
+        new_dir = return_new_path(dir)
+        renames[dir] = new_dir
+    else:
+        dir = Path('.')
+
     if dir.exists():
+        # Return a list of all paths of dir/files in nested directories 
         paths = [x for x in dir.glob(f'**/{old_string}*')]
         paths.insert(0, dir.resolve())
         for path in paths:
             if path.parent in renames.keys():
-                new_path = (renames[path.parent]).joinpath(f'{new_string}' + path.name[3:])
+                new_path = return_new_path(path)
             else:
-                new_path = path.parent.joinpath(f'{new_string}' + path.name[3:])
+                new_path = return_new_path(path)
             if path.is_dir():
                 renames[path] = new_path
-                new_path.mkdir(parents=True, exist_ok=True) 
+                new_path.mkdir(parents=True, exist_ok=True)
             else:
                 path.replace(new_path)
     else:
@@ -48,9 +68,7 @@ subdirectories/files, all with a specified letter prefix (see below).
     parser.add_argument(
         'dir',
         help="Optional argument that points to directory where files and folders are to be copied",
-        nargs='*',
-        default='.'
+        nargs='?',
     )
     args = parser.parse_args()
     main(**vars(args))
-
